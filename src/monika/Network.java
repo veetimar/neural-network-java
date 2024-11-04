ef6480027244697e13fddbbef8a04a3f789b75ea
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2024-11-04 16:02:51 +0200

    Backpropagation

--- src/monika/Network.java
+++ src/monika/Network.java
@@ -1,6 +1,6 @@
 package monika;
 import java.util.*;
-//TODO: backward (sgd)
+// TODO: genetic algorithm?, Constructor that only takes a file?, class hierarchy?
 public class Network {
     private final Layer[] layers;
     private final float learningRate = 0.01f;
@@ -18,30 +18,33 @@ public class Network {
                 this.layers[i] = new Layer(size[i], size[i - 1], true);
             }
         }
+        System.gc();
     }
 
     public float[] forward(float[] inputs) {
-        if (inputs.length != layers[0].getLength()) {
+        if (inputs.length != layers[0].length()) {
             throw new IllegalArgumentException("The size of the inputs array does not match the size of the input layer!");
         }
         for (int i = 0; i < inputs.length; i++) {
-            layers[0].getNeuron(i).setValue(inputs[i]);
+            layers[0].neuron(i).setActivation(inputs[i]);
         }
-        float[] outputs = new float[layers[layers.length - 1].getLength()];
+        float[] outputs = new float[layers[layers.length - 1].length()];
         for (int i = 1; i < layers.length; i++) {
             boolean output = i == layers.length - 1;
-            for (int j = 0; j < layers[i].getLength(); j++) {
+            for (int j = 0; j < layers[i].length(); j++) {
+                Neuron cn = layers[i].neuron(j);
                 float value = 0;
-                for (int k = 0; k < layers[i - 1].getLength(); k++) {
-                    value += layers[i - 1].getNeuron(k).getValue() * layers[i].getNeuron(j).getWeight(k);
+                for (int k = 0; k < layers[i - 1].length(); k++) {
+                    value += layers[i - 1].neuron(k).getActivation() * cn.getWeight(k);
                 }
-                value += layers[i].getNeuron(j).getBias();
+                value += cn.getBias();
+                cn.setValue(value);
                 if (!output) {
                     value = elu(value);
-                    layers[i].getNeuron(j).setValue(value);
+                    cn.setActivation(value);
                 } else {
                     value = sigmoid(value);
-                    layers[i].getNeuron(j).setValue(value);
+                    cn.setActivation(value);
                     outputs[j] = value;
                 } 
             }
@@ -63,20 +66,36 @@ public class Network {
     }
 
     public float backward(float[] inputs, float[] expectedOutputs) {
-        if (inputs.length != layers[0].getLength()) {
+        if (inputs.length != layers[0].length()) {
             throw new IllegalArgumentException("The size of the inputs array does not match the size of the input layer!");
-        } else if (expectedOutputs.length != layers[layers.length - 1].getLength()) {
+        } else if (expectedOutputs.length != layers[layers.length - 1].length()) {
             throw new IllegalArgumentException("The size of the expected outputs array does not match the size of the output layer!");
         }
         float[] outputs = forward(inputs);
         float error = meanSquaredError(outputs, expectedOutputs);
-
-
-        for (int i = layers.length - 1;i > 0; i-- ) {
-
+        float[] errorpd = squaredErrorPD(outputs, expectedOutputs);
+        for (int i = 0; i < layers[layers.length - 1].length(); i++) {
+            layers[layers.length - 1].neuron(i).setGradient(errorpd[i]);
         }
-
-
+        for (int i = layers.length - 1; i > 0; i-- ) {
+            boolean output = i == layers.length - 1;
+            for (int j = 0; j < layers[i].length(); j++) {
+                Neuron cn = layers[i].neuron(j);
+                float gradientd;
+                if (!output) {
+                    gradientd = cn.getGradient() * eluD(cn.getValue());
+                } else {
+                    gradientd = cn.getGradient() * sigmoidD(cn.getValue());
+                }
+                cn.setBias(cn.getBias() - learningRate * gradientd);
+                for (int k = 0; k < layers[i - 1].length(); k++) {
+                    cn.setWeight(k, cn.getWeight(k) - learningRate * layers[i - 1].neuron(k).getActivation() * gradientd);
+                    if (i > 1) {
+                        layers[i - 1].neuron(k).setGradient(layers[i - 1].neuron(k).getGradient() + cn.getWeight(k) * gradientd);
+                    }
+                }
+            }
+        }
         update();
         return error;
     }
@@ -89,13 +108,15 @@ public class Network {
         for (int i = 0; i < outputs.length; i++) {
             x += Math.pow(outputs[i] - expectedOutputs[i], 2);
         }
-        return 1.0f / outputs.length * x;
+        return x / outputs.length;
     }
 
-    private void update() {
-        for (int i = 1; i < layers.length; i++) {
-            layers[i].update();
+    private float[] squaredErrorPD(float[] outputs, float[] expectedOutputs) {
+        float[] derivatives = new float[outputs.length];
+        for (int i = 0; i < derivatives.length; i++) {
+            derivatives[i] = 2 * (outputs[i] - expectedOutputs[i]);
         }
+        return derivatives;
     }
 
     private float sigmoid(float x) {
@@ -122,11 +143,17 @@ public class Network {
         }
     }
 
-    public Layer getLayer(int index) {
+    public void update() {
+        for (int i = 1; i < layers.length; i++) {
+            layers[i].update();
+        }
+    }
+
+    public Layer layer(int index) {
         return layers[index];
     }
 
-    public int getLength() {
+    public int length() {
         return layers.length;
     }
 
@@ -173,21 +200,21 @@ class Layer {
         }
     }
 
-    private float[] he(int inputs) {
-        float[] weights = new float[inputs];
+    private float[] he(int size) {
+        float[] weights = new float[size];
         Random r = new Random();
-        float stddev = (float)Math.sqrt(2.0 / inputs);
+        float stddev = (float)Math.sqrt(2.0 / size);
         for (int i = 0; i < weights.length; i++) {
             weights[i] = (float)r.nextGaussian(0, stddev);
         }
         return weights;
     }
 
-    private float[] xavier(int inputs) {
-        float[] weights = new float[inputs];
+    private float[] xavier(int size) {
+        float[] weights = new float[size];
         Random r = new Random();
-        float min = (float)(-1.0 / Math.sqrt(inputs));
-        float max = (float)(1.0 / Math.sqrt(inputs));
+        float min = (float)(-1.0 / Math.sqrt(size));
+        float max = (float)(1.0 / Math.sqrt(size));
         for (int j = 0; j < weights.length; j++) {
             weights[j] = r.nextFloat(min, max);
         }
@@ -200,11 +227,11 @@ class Layer {
         }
     }
 
-    public Neuron getNeuron(int index) {
+    public Neuron neuron(int index) {
         return neurons[index];
     }
 
-    public int getLength() {
+    public int length() {
         return neurons.length;
     }
 
@@ -222,13 +249,16 @@ class Layer {
 
 class Neuron {
     private float value;
+    private float activation;
     private float bias;
     private float cacheBias;
-    private float[] weights;
-    private float[] cacheWeights;
+    private final float[] weights;
+    private final float[] cacheWeights;
     private float gradient;
 
     public Neuron() {
+        this.weights = null;
+        this.cacheWeights = null;
     }
 
     public Neuron(float[] weights, float bias) {
@@ -246,12 +276,14 @@ class Neuron {
             weights[i] = cacheWeights[i];
         }
         gradient = 0;
-        cacheBias = 0;
-        Arrays.fill(cacheWeights, 0);
     }
 
     public void setValue(float value) {
         this.value = value;
+    }
+
+    public void setActivation(float activation) {
+        this.activation = activation;
     }
 
     public void setBias(float bias) {
@@ -270,6 +302,10 @@ class Neuron {
         return value;
     }
 
+    public float getActivation() {
+        return activation;
+    }
+
     public float getBias() {
         return bias;
     }
@@ -283,6 +319,6 @@ class Neuron {
     }
 
     public String toString() {
-        return String.valueOf(value);
+        return String.valueOf(activation);
     }
 }
