bbeba3018b26a6dd536f2c48ab11e598e04abe76
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-01-09 23:08:43 +0200

    Organize training and testing into functions

--- src/Run.java
+++ src/Run.java
@@ -1,18 +1,27 @@
 import monika.*;
 
+// A simple program for testing the functionality of the neural network (XOR gate)
 public class Run {
+    static Network network;
 
-    // XOR gate
     public static void main(String[] args) {
-        Network network = new Network(2, 3, 1);
+        network = new Network(2, 3, 1);
+        train_xor();
+        test_xor();
+    }
+
+    public static void train_xor() {
+        System.out.println("Training started.");
         float[][][] trainingData = {{{0, 0}, {0}}, {{0, 1}, {1}}, {{1, 0}, {1}}, {{1, 1}, {0}}};
         float[] errors = network.train(trainingData, 10000, 2, 0.1f);
-        System.out.println("Training started.");
         for (int i = 0; i < errors.length; i++) {
             System.out.printf("Epoch " + (i + 1) + " mean error: %f", errors[i]);
             System.out.println();
         }
         System.out.println("Training finished.");
+    }
+
+    public static void test_xor() {
         System.out.println();
         System.out.println("In: " + "0  0");
         System.out.printf("Out: %f", network.forward(0, 0)[0]);
